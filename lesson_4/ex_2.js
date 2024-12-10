4b87af5ea7a4abe9461c2525dc4436d8081b4fe6
shoghdev <shogher.fordev@gmail.com>
2024-12-11 00:20:28 +0400

    Read File Content into a Buffer

--- lesson_4/ex_2.js
+++ lesson_4/ex_2.js
@@ -7,3 +7,9 @@ const updateFile = fs.writeSync(file, "Hello, this is the first line.\n")
 const appendText = fs.writeSync(file, "This is the second line.\n", "a")
 fs.closeSync(file)
 
+//Task 2: Read File Content into a Buffer
+const openData = fs.openSync("data.txt", 'r')
+const buffer = Buffer.alloc(64)
+const bytesRead = fs.readSync(openData, buffer, 0, buffer.length, null)
+console.log(buffer)
+fs.closeSync(openData)
