07a0a0c11c5d45cbb02fa35902c8006df3c9b85b
shoghdev <shogher.fordev@gmail.com>
2024-12-11 00:22:44 +0400

    Copy File Content Byte-by-Byte

--- lesson_4/ex_2.js
+++ lesson_4/ex_2.js
@@ -13,3 +13,17 @@ const buffer = Buffer.alloc(64)
 const bytesRead = fs.readSync(openData, buffer, 0, buffer.length, null)
 console.log(buffer)
 fs.closeSync(openData)
+
+//Task 3: Copy File Content Byte-by-Byte
+const sourceFile = fs.openSync("data.txt", "r")
+const destinationFile = fs.openSync("copy.txt", "w")
+const newBuffer = Buffer.alloc(16)
+let bytesForRead
+do {
+    bytesForRead = fs.readSync(sourceFile, buffer, 0, buffer.length, null)
+    if (bytesForRead > 0) {
+        fs.writeSync(destinationFile, buffer, 0, bytesForRead)
+    }
+} while (bytesForRead > 0)
+console.log(newBuffer)
+fs.closeSync(sourceFile, destinationFile)
