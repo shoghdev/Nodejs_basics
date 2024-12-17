6e536b3de563cedca596d8042c623378a8a1ec4a
shoghdev <shogher.fordev@gmail.com>
2024-12-17 22:44:13 +0400

    Add ; to te end of lines

--- lesson_7/_serve_htmtl.js
+++ lesson_7/_serve_htmtl.js
@@ -1,14 +1,14 @@
-const http = require("node:http")
-const fs = require("node:fs")
-const path = require("node:path")
+const http = require("node:http");
+const fs = require("node:fs");
+const path = require("node:path");
 const server = http.createServer((req, res)=>{
-    const filePath = path.join(__dirname, "index.html")
-    res.writeHead(200, {'Content-type' : 'text/html'})
-    const read = fs.readFileSync(filePath,"utf8")
-    res.end(read)
+    const filePath = path.join(__dirname, "index.html");
+    res.writeHead(200, {'Content-type' : 'text/html'});
+    const read = fs.readFileSync(filePath,"utf8");
+    res.end(read);
 })
 
-const PORT = 3000
+const PORT = 3000;
 
 server.listen(PORT, ()=>{
     console.log(`Server is runing on ${PORT}`);
